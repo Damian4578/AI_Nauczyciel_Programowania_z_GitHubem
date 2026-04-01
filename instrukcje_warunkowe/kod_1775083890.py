35f1115aa3a022fef09167da3c87f6c656f44981
Damian4578 <54469294+Damian4578@users.noreply.github.com>
2026-04-02 00:51:30 +0200

    Lekcja: Instrukcje Warunkowe

---
+++ instrukcje_warunkowe/kod_1775083890.py
@@ -0,0 +1,10 @@
+```python
+# Przykład z lądowaniem Falcona 9
+poziom_paliwa = 30  
+if poziom_paliwa > 50:
+    print("Lądowanie normalne")
+elif 20 <= poziom_paliwa <= 50:
+    print("Włączono tryb oszczędności paliwa")
+else:
+    print("Awaryjne lądowanie")
+```
