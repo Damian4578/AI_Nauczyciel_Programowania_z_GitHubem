25462b051416cf9cbf9ab1fd6ee0e8a44788cb62
Damian4578 <54469294+Damian4578@users.noreply.github.com>
2026-04-02 00:50:26 +0200

    Delete instrukcje_warunkowe directory

--- instrukcje_warunkowe/kod_1775083236.py
+++
@@ -1,8 +0,0 @@
-```python
-if warunki_pogodowe == "słoneczne":
-    print("Rakietę kierujemy na trajektorię słoneczną.")
-elif warunki_pogodowe == "zachmurzone":
-    print("Rakietę kierujemy na trajektorię awaryjną.")
-else:
-    print("Rakietę kierujemy na standardową trajektorię.")
-```
