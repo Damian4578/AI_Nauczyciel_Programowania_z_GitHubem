774b42ca0c013bf269dd1e7d590e9cbafcf9125c
Damian4578 <54469294+Damian4578@users.noreply.github.com>
2026-04-02 00:57:10 +0200

    Lekcja: Instrukcje Warunkowe w Pythonie

---
+++ instrukcje_warunkowe_w_pythonie/kod_1775084230.py
@@ -0,0 +1,12 @@
+```python
+# przykład kodu
+x = 5
+if x > 10:
+    print("x jest większe niż 10")
+elif x == 5:
+    print("x jest równe 5")
+elif x < 0:
+    print("x jest mniejsze niż 0")
+else:
+    print("x jest równe 0")
+```
