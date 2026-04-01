0336c50bd5e62c6b061091e6af9c1e8dbc4b78f4
Damian4578 <54469294+Damian4578@users.noreply.github.com>
2026-04-01 23:48:35 +0200

    Lekcja: Logika i Sterowanie w Pythonie

---
+++ logika_i_sterowanie_w_pythonie/kod_1775080115.py
@@ -0,0 +1,7 @@
+```python
+x = 5  # przypisanie wartości do zmiennej
+if x > 5:  # instrukcja warunkowa
+    print("x jest większe niż 5")
+elif x == 5:  # dodatkowa instrukcja warunkowa
+    print("x jest równy 5")
+```
